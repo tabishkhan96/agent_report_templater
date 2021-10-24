7b7fae389c60d236b84c48c5294fa194550dbb5f
alex.superdeveloper <antonov.alex94@mail.ru>
2021-10-24 14:49:22 +0300

    refactored strategies

--- src/repository/report_strategies.py
+++ src/repository/report_strategies.py
@@ -87,65 +87,20 @@ class SelfImportReportCreationStrategy(ReportCreationStrategyInterface):
         if not calibre_table or not conclusion_table or not shelf_life_table or not executor_table:
             raise DocumentTemplateCorruptedException('Отсутствует один из шаблонов таблиц заключения')
 
-        # TODO following tables
-
-
-        calibre_table.column_cells(0)[1].text = next(unit.number for unit in self.report.transport_units)
-        report_doc.set_cell_style(calibre_table.column_cells(0)[1])
-        shelf_life_table.column_cells(0)[1].text = next(unit.number for unit in self.report.transport_units)
-        report_doc.set_cell_style(shelf_life_table.column_cells(0)[1])
-        executor_table.column_cells(0)[3].text = datetime.today().strftime("%d.%m.%Y")
-        report_doc.set_cell_style(executor_table.column_cells(0)[3])
-
-        for i, TU_number in enumerate([unit.number for unit in self.report.transport_units][1:]):
-            row: Row = calibre_table.add_row()
-            row.cells[0].text = TU_number
-            report_doc.set_cell_style(row.cells[0])
-
-            row: Row = shelf_life_table.add_row()
-            row.cells[0].text = TU_number
-            report_doc.set_cell_style(row.cells[0])
-
-        report_doc.append_table(calibre_table)
-        report_doc.append_table(conclusion_table)
-        report_doc.append_table(shelf_life_table)
+        self.add_calibre_table(report_doc, calibre_table)
+        self.add_conclusion_table(report_doc, conclusion_table)
+        self.add_shelf_life_table(report_doc, shelf_life_table)
+        TemplateEngine.replace_in_table(
+            table=executor_table, values=self.report, cell_handler=self.document_dao.set_cell_style
+        )
         report_doc.append_table(executor_table)
 
         report_doc.add_page_break()
-
-        for TU_number in self.report.transport_units_numbers():
-            for _ in range(2):
-                paragraph = report_doc.append_paragraph(f"Контейнер: {TU_number}\nНомер датчика:\n")
-                paragraph.runs[0].bold = True
-                paragraph.runs[0].font.name = "Times New Roman"
-            report_doc.add_page_break()
-
-        doc_guid: str = uuid.uuid4().hex
-        filename: str = f"{doc_guid}_{self.report.number}_{self.report.order}_" \
-                        f"{self.report.inspection_date}".replace("/", '')
-
-        report_doc.save(f"{settings.REPOSITORY.REPORTS_DIR}/{filename}.{settings.DOC_TYPE}")
-        self.logger.info(f"Doc saved to '{settings.REPOSITORY.REPORTS_DIR}/' with GUID {doc_guid}.")
-        return doc_guid
+        self.add_pictures_of_thermographs(report_doc)
+        return report_doc
 
     def add_temperature_table(self, report_doc: DocumentDAOInterface, temperature_table: Table):
-        cells_content: list[str] = []
-        containers: Generator[Container] = (container for container in self.report.transport_units)
-        first_container: Container = next(containers, {})
-
-        for cell in temperature_table.rows[-1].cells:
-            cells_content.append(cell.text)
-            cell.text = TemplateEngine.replace_text(cell.text, first_container)
-
-        for container in containers:
-            row: Row = temperature_table.add_row()
-            for number, cell in enumerate(row.cells):
-                cell.text = TemplateEngine.replace_text(cells_content[number], container)
-                report_doc.set_cell_style(cell)
-
-        TemplateEngine.replace_in_table(
-            table=temperature_table, values=self.report, cell_handler=report_doc.set_cell_style
-        )
+        self._fill_table_with_row_for_container(self.report.transport_units, temperature_table)
         report_doc.append_table(temperature_table)
 
     def add_tally_account_and_pallets_tables(
@@ -182,19 +137,43 @@ class SelfImportReportCreationStrategy(ReportCreationStrategyInterface):
                 tbl_number = 0
             table = list(inspection_result_template.get_tables())[tbl_number]
 
-            cells_content: list[str] = []
-            containers: Generator[Container] = (container for container in containers)
-            first_container: Container = next(containers, {})
-
-            for cell in table.rows[-1].cells:
-                cells_content.append(cell.text)
-                cell.text = TemplateEngine.replace_text(cell.text, first_container)
-
-            for container in containers:
-                row: Row = table.add_row()
-                for number, cell in enumerate(row.cells):
-                    cell.text = TemplateEngine.replace_text(cells_content[number], container)
-                    report_doc.set_cell_style(cell)
-
-            TemplateEngine.replace_in_table(table=table, values=self.report, cell_handler=report_doc.set_cell_style)
+            self._fill_table_with_row_for_container(containers, table)
             report_doc.append_table(table)
+
+    def add_calibre_table(self, report_doc: DocumentDAOInterface, calibre_table: Table):
+        self._fill_table_with_row_for_container(self.report.transport_units, calibre_table)
+        report_doc.append_table(calibre_table)
+
+    def add_conclusion_table(self, report_doc, conclusion_table):
+        report_doc.append_table(conclusion_table)
+
+    def add_shelf_life_table(self, report_doc, shelf_life_table):
+        self._fill_table_with_row_for_container(self.report.transport_units, shelf_life_table)
+        report_doc.append_table(shelf_life_table)
+
+    def add_pictures_of_thermographs(self, report_doc):
+        for TU in self.report.transport_units:
+            for thermograph in TU.temperature.thermographs:
+                paragraph = report_doc.append_paragraph(f"Контейнер: {TU.number}\nНомер датчика:{thermograph.number}\n")
+                paragraph.runs[0].bold = True
+                paragraph.runs[0].font.name = "Times New Roman"
+                if thermograph.graph:
+                    report_doc.append_paragraph().add_run().add_picture(thermograph.graph)
+            report_doc.add_page_break()
+
+    def _fill_table_with_row_for_container(self, containers: list[Container], table: Table):
+        cells_content: list[str] = []
+        containers: Generator[Container] = (container for container in containers)
+        first_container: Container = next(containers, {})
+
+        for cell in table.rows[-1].cells:
+            cells_content.append(cell.text)
+            cell.text = TemplateEngine.replace_text(cell.text, first_container)
+
+        for container in containers:
+            row: Row = table.add_row()
+            for number, cell in enumerate(row.cells):
+                cell.text = TemplateEngine.replace_text(cells_content[number], container)
+                self.document_dao.set_cell_style(cell)
+
+        TemplateEngine.replace_in_table(table=table, values=self.report, cell_handler=self.document_dao.set_cell_style)
